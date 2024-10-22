c9b78ec37b037abec8b68e89577f8f432f59937d
NicolasRR <nreateguir@gmail.com>
2024-10-22 11:20:02 +0200

    new experiments

--- head_ablations/utils.py
+++ head_ablations/utils.py
@@ -1,6 +1,13 @@
 import torch
 from tqdm import tqdm
-
+import numpy as np
+from transformers.models.phi3.modeling_phi3 import Phi3Attention,apply_rotary_pos_emb,repeat_kv
+from typing import Optional, Tuple
+from transformers.cache_utils import Cache
+from torch import nn
+import math
+import logging
+logger = logging.get_logger(__name__)
 
 def get_importance_score(model, dataloader, assistant_token=32001):
     model.eval()
@@ -41,3 +48,130 @@ def get_dataloader(dataset, tokenizer, batch_size=1, max_length=4096):
     columns_to_remove = ["user", "assistant", "role"]
     dataset = dataset.remove_columns(columns_to_remove)
     return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+def get_prefix_matching_scores(model, filtered_ranks, layers, heads, num_seeds=100):
+    prefix_matching = []
+    with torch.no_grad():
+        for seed in tqdm(range(1,num_seeds)):
+            L = 2*seed+23
+            np.random.seed(seed)
+            X = np.random.choice(filtered_ranks, size=L, replace=False)
+            X = torch.tensor(np.repeat(X, 4))
+            attentions = torch.cat(model(torch.tensor(X).unsqueeze(0).to(model.device),output_attentions=True).attentions)
+            scores = torch.zeros((layers, heads))
+            for l in range(layers):
+                for h in range(heads):
+                    att = attentions[l][h]
+                    for token_idx in range(L+1, 4*L):
+                        att_token = att[token_idx]
+                        previous_tokens = X[:token_idx]
+                        token = X[token_idx]
+                        idx = (previous_tokens == token).nonzero()
+                        for i in idx:
+                            prefix_score = att_token[i+1]
+                            scores[l,h] += prefix_score.item()
+                    scores[l,h] /= 3*L
+            prefix_matching.append(scores)
+    return torch.stack(prefix_matching)
+
+class AttentionAblation(Phi3Attention):
+    def __init__(self, config, layer_idx, ablate=False, ablate_idx=None):
+        super().__init__(config, layer_idx)
+        self.ablate = ablate
+        self.ablate_idx = ablate_idx
+
+    def forward(
+            self,
+            hidden_states: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.LongTensor] = None,
+            past_key_value: Optional[Cache] = None,
+            output_attentions: bool = False,
+            use_cache: bool = False,
+        ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+            logger.warning_once("You are not running the flash-attention implementation, expect numerical differences.")
+
+            bsz, q_len, _ = hidden_states.size()
+
+            qkv = self.qkv_proj(hidden_states)
+            query_pos = self.num_heads * self.head_dim
+            query_states = qkv[..., :query_pos]
+            key_states = qkv[..., query_pos : query_pos + self.num_key_value_heads * self.head_dim]
+            value_states = qkv[..., query_pos + self.num_key_value_heads * self.head_dim :]
+
+            query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+            key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+            value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+            kv_seq_len = key_states.shape[-2]
+            if past_key_value is not None:
+                if self.layer_idx is None:
+                    raise ValueError(
+                        f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__} "
+                        "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
+                        "with a layer index."
+                    )
+                kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+            cos, sin = self.rotary_emb(value_states, position_ids, seq_len=kv_seq_len)
+
+            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+            if past_key_value is not None:
+                cache_kwargs = {"sin": sin, "cos": cos}  # Specific to RoPE models
+                key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+            # repeat k/v heads if n_kv_heads < n_heads
+            key_states = repeat_kv(key_states, self.num_key_value_groups)
+            value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+            attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+            if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+                raise ValueError(
+                    f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+                    f" {attn_weights.size()}"
+                )
+
+            if attention_mask is not None:
+                if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                    raise ValueError(
+                        f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                    )
+                attn_weights = attn_weights + attention_mask
+
+            # upcast attention to fp32
+
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(value_states.dtype)
+            attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+
+            attn_output = torch.matmul(attn_weights, value_states)
+            ############################
+            if self.ablate and self.ablate_idx is not None:
+                for i in self.ablate_idx:
+                    attn_output[:,i,:,:] = 0.0
+            ############################
+
+            if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+                raise ValueError(
+                    f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                    f" {attn_output.size()}"
+                )
+
+            attn_output = attn_output.transpose(1, 2).contiguous()
+            attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+            attn_output = self.o_proj(attn_output)
+
+            if not output_attentions:
+                attn_weights = None
+
+            return attn_output, attn_weights, past_key_value
+    
+def get_ablated_model(model):
+
+    for i in range(model.config.num_hidden_layers):
+        attn = model.model.layers._modules[str(i)].self_attn
+        self_attention = AttentionAblation(attn.config,attn.layer_idx, ablate=False, ablate_idx=[]).to(model.dtype).to(model.device)
+        self_attention.load_state_dict(attn.state_dict())
+        model.model.layers._modules[str(i)].self_attn = self_attention 
+    return model
